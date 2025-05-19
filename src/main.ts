754a512607db97b0fc18a2f3627f3939d80d8750
Ian Carlos Ortega <iancarlosortegaleon@gmail.com>
2025-05-19 17:17:19 -0500

    feat: setup open api documentation on main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
@@ -21,6 +22,16 @@ async function bootstrap() {
 			forbidNonWhitelisted: true,
 		}),
 	);
+
+	const config = new DocumentBuilder()
+		.setTitle('Tasks API')
+		.setDescription(
+			'This API provides a simple task management system. You can create, update, delete, and retrieve tasks. Each endpoint is secured with JWT authentication and requires a valid token to access.',
+		)
+		.setVersion('1.0')
+		.build();
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api', app, document);
 
 	await app.listen(port);
 	logger.log(`Application is running on: ${port}`);
