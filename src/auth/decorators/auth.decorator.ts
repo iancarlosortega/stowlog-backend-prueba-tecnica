3195ff0545a0bf7670cc4acd0e48532eee61b764
Ian Carlos Ortega <iancarlosortegaleon@gmail.com>
2025-05-19 16:33:20 -0500

    fix: correct import path for roles constant

--- src/auth/decorators/auth.decorator.ts
+++ src/auth/decorators/auth.decorator.ts
@@ -1,7 +1,7 @@
 import { SetMetadata, UseGuards, applyDecorators } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { UserRoleGuard } from '@/auth/guards/user-role.guard';
-import { META_ROLES, UserRoles } from '@/auth/constants/ROLES';
+import { META_ROLES, UserRoles } from '@/auth/constants/roles';
 
 export function Auth(...roles: UserRoles[]) {
 	return applyDecorators(
--- src/auth/guards/user-role.guard.ts
+++ src/auth/guards/user-role.guard.ts
@@ -7,7 +7,7 @@ import {
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
-import { META_ROLES } from '@/auth/constants/ROLES';
+import { META_ROLES } from '@/auth/constants/roles';
 
 @Injectable()
 export class UserRoleGuard implements CanActivate {
