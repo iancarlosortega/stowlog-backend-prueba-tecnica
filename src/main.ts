56a3e1d099e9a8df9cebcf5afb2c3ac3e1915e38
Ian Carlos Ortega <iancarlosortegaleon@gmail.com>
2025-05-19 12:50:49 -0500

    feat: define api prefix global and validation pipes for dtos

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+	const logger = new Logger('Bootstrap');
+
+	const port = process.env.PORT;
+	if (!port) {
+		throw new Error('PORT is not defined');
+	}
+
+	const app = await NestFactory.create(AppModule);
+
+	app.enableCors();
+	app.setGlobalPrefix('api');
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			forbidNonWhitelisted: true,
+		}),
+	);
+
+	await app.listen(port);
+	logger.log(`Application is running on: ${port}`);
 }
 bootstrap();
